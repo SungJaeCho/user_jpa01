b5bfffcc808d452068da5c113d4610e6baefa4c4
sjchoRsquare <sjcho@rsquare.co.kr>
2022-07-22 08:18:39 +0900

    엔티티 생성시 주의점 블로그에 기술해놓음
    연관관계 메서드 활용

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,14 +16,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id") //연관관계의 주인은 orders table
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne // 1:1은 연관관계의 주인을 잘 선택해야함 지금같은경우 order를 통해 delivery를 볼일이 많기때문에 order가 연관관계의 주인이 된다.
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // 1:1은 연관관계의 주인을 잘 선택해야함 지금같은경우 order를 통해 delivery를 볼일이 많기때문에 order가 연관관계의 주인이 된다.
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -31,4 +31,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    //==연관관계 메서드, 컨트롤 하는쪽에서 메서드를 가지는게 좋음==/
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
